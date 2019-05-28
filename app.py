8132cfc817cf6c071ba530b88a48c51c93dab7f8
KatieKEK <50963062+KatieKEK@users.noreply.github.com>
2019-05-28 13:21:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Bananas are great for small children!"    
+
+@app.route("/html")
+def html():
+    return "<p>Bananas are great for small children!</p>"  
+
+@app.route("/")
+def monkey():
+    return "monkey's will in turn eat small children"  
     
 if __name__ == '__main__':
     app.run(use_reloader=True)
