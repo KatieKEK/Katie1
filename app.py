4b7db0e6ed148fc876f7c4e7bdef776496ac3d71
KatieKEK <50963062+KatieKEK@users.noreply.github.com>
2019-05-28 13:53:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import Flask
 app = Flask(__name__)
+static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static')
+
 
 @app.route("/")
 def index():
     return "Bananas are great for small children!"    
 
-@app.route("/html")
-def html():
-    return "<p>Bananas are great for small children!</p>"  
+@app.route("/images/Liptak_Agent_Picture.png")
+def Liptak_Agent_Picture():
+    return send_from_directory(static_file_dir, 'Liptak_Agent_Picture.png')
+
+@app.route("/images/eye_icon.ico")
+def icon():
+    return send_from_directory(static_file_dir, 'eye_icon.ico')
+
+@app.route("/images/Liptak_Agent_Picture.png")
+def Liptak_Agent_Picture():
+    return send_from_directory(static_file_dir, 'Liptak_Agent_Picture.png')
 
 @app.route("/monkeys")
 def monkey():
